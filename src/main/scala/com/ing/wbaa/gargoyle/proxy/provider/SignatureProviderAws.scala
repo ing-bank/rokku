756d64ccca27bb7d66ecde604df80cff7659b2e4
arempter <arempter@gmail.com>
2018-10-02 17:00:28 +0200

    v2 and v4 with tests working, no cleanup

--- src/main/scala/com/ing/wbaa/gargoyle/proxy/provider/SignatureProviderAws.scala
+++ src/main/scala/com/ing/wbaa/gargoyle/proxy/provider/SignatureProviderAws.scala
@@ -4,8 +4,9 @@ import java.net.URI
 
 import akka.http.scaladsl.model.HttpRequest
 import com.amazonaws.DefaultRequest
-import com.amazonaws.auth.{ AWS3Signer, AWS4Signer, BasicAWSCredentials }
+import com.amazonaws.auth.{ AWS4Signer, BasicAWSCredentials }
 import com.amazonaws.http.HttpMethodName
+import com.amazonaws.services.s3.internal.S3Signer
 import com.ing.wbaa.gargoyle.proxy.data.AwsSecretKey
 import com.typesafe.scalalogging.LazyLogging
 
@@ -15,7 +16,7 @@ trait SignatureProviderAws extends LazyLogging {
   lazy val signer = new AWS4Signer()
 
   //move to data
-  case class AWSHeaderValues(accessKey: String, signedHeaders: String, signature: String, contentSHA256: String, requestDate: String, securityToken: String, version: String)
+  case class AWSHeaderValues(accessKey: String, signedHeaders: String, signature: String, contentSHA256: String, requestDate: String, securityToken: String, version: String, userAgent: String)
 
   // Map[String, util.List[String]] is need by AWS4Signer
   private def extractRequestParameters(httpRequest: HttpRequest) = {
@@ -50,12 +51,21 @@ trait SignatureProviderAws extends LazyLogging {
     }
   }
 
-  private def getAWSHeaders(httpRequest: HttpRequest) = {
-    val authorization = httpRequest.getHeader("authorization").get().value()
-    val signature =
+  def getSignature(authorization: String) = {
+    if (authorization.contains("AWS4")) {
       """\S+ Signature=(\S+)""".r
         .findFirstMatchIn(authorization)
-        .map(_ group 1)
+        .map(_ group 1).getOrElse("")
+    } else {
+      """AWS (\S+):(\S+)""".r
+        .findFirstMatchIn(authorization)
+        .map(_ group 2).getOrElse("")
+    }
+  }
+
+  private def getAWSHeaders(httpRequest: HttpRequest) = {
+    val authorization = httpRequest.getHeader("authorization").get().value()
+    val signature = getSignature(authorization)
 
     val signedHeaders =
       """\S+ SignedHeaders=(\S+), """.r
@@ -69,23 +79,32 @@ trait SignatureProviderAws extends LazyLogging {
 
     val version = if (authorization.contains("AWS4")) { "v4" } else { "v2" }
     val contentSHA256 = if (version == "v4") { httpRequest.getHeader("X-Amz-Content-SHA256").get().value() } else { "" }
-    val requestDate = if (version == "v4") { httpRequest.getHeader("X-Amz-Date").get().value() } else { "" }
+    val requestDate = if (version == "v4") {
+      httpRequest.getHeader("X-Amz-Date").get().value()
+    } else {
+      logger.debug("getting date from request v2 " + httpRequest.getHeader("Date").get().value())
+      httpRequest.getHeader("Date").get().value()
+    }
     val securityToken = httpRequest.getHeader("X-Amz-Security-Token").get().value()
+    val userAgent = httpRequest.getHeader("User-Agent").get().value()
 
-    logger.debug("Original request headers: " + AWSHeaderValues(accessKey, signedHeaders, signature.getOrElse(""), contentSHA256, requestDate, securityToken, version))
-    AWSHeaderValues(accessKey, signedHeaders, signature.getOrElse(""), contentSHA256, requestDate, securityToken, version)
+    logger.debug("Original request headers: " + AWSHeaderValues(accessKey, signedHeaders, signature, contentSHA256, requestDate, securityToken, version, userAgent))
+    AWSHeaderValues(accessKey, signedHeaders, signature, contentSHA256, requestDate, securityToken, version, userAgent)
   }
+
+  private def getMethod(httpRequest: HttpRequest) =
+    httpRequest.method.value match {
+      case "GET"    => HttpMethodName.GET
+      case "POST"   => HttpMethodName.POST
+      case "PUT"    => HttpMethodName.PUT
+      case "DELETE" => HttpMethodName.DELETE
+    }
 
   private def getSignableRequest(httpRequest: HttpRequest, request: DefaultRequest[_] = new DefaultRequest("s3")): DefaultRequest[_] = {
     import scala.collection.JavaConverters._
 
     // add values to Default request for signature
-    request.setHttpMethod(httpRequest.method.value match {
-      case "GET"    => HttpMethodName.GET
-      case "POST"   => HttpMethodName.POST
-      case "PUT"    => HttpMethodName.PUT
-      case "DELETE" => HttpMethodName.DELETE
-    })
+    request.setHttpMethod(getMethod(httpRequest))
     request.setResourcePath(httpRequest.uri.path.toString())
     request.setEndpoint(
       new URI(s"http://${httpRequest.uri.authority.toString()}"
@@ -103,7 +122,9 @@ trait SignatureProviderAws extends LazyLogging {
     // todo: do we want v3?
     version match {
       case "v2" =>
-        val singer = new AWS3Signer()
+        logger.debug("v2 params: " + request.getHttpMethod.toString + " " + request.getResourcePath)
+        val resourcePath = request.getResourcePath
+        val singer = new S3Signer(request.getHttpMethod.toString, resourcePath)
         singer.sign(request, cred)
 
       case "v4" =>
@@ -118,6 +139,7 @@ trait SignatureProviderAws extends LazyLogging {
     val awsHeaders = getAWSHeaders(httpRequest)
     val credentials = new BasicAWSCredentials(awsHeaders.accessKey, awsSecretKey.value)
     val incomingRequest = getSignableRequest(httpRequest)
+    val authorization = httpRequest.getHeader("authorization").get().value()
 
     // add headers from original request before sign
     incomingRequest.addHeader("X-Amz-Security-Token", awsHeaders.securityToken)
@@ -125,20 +147,21 @@ trait SignatureProviderAws extends LazyLogging {
       incomingRequest.addHeader("SignedHeaders", awsHeaders.signedHeaders)
       incomingRequest.addHeader("X-Amz-Content-SHA256", awsHeaders.contentSHA256)
     }
+    if (awsHeaders.version == "v2") {
+      incomingRequest.addHeader("Content-Type", httpRequest.entity.contentType.mediaType.value)
+      incomingRequest.addHeader("Date", awsHeaders.requestDate)
+    }
 
     // generate new signature
     signS3Request(incomingRequest, credentials, awsHeaders.version)
 
     logger.debug("Signed Request:" + incomingRequest.getHeaders.toString)
 
-    val newSignature =
-      """\S+ Signature=(\S+)""".r
-        .findFirstMatchIn(httpRequest.getHeader("authorization").get().value())
-        .map(_ group 1)
+    val newSignature = getSignature(authorization)
 
-    logger.debug(s"New Signature ${newSignature.getOrElse("")} Original Signature: ${awsHeaders.signature}")
+    logger.debug(s"New Signature ${newSignature} Original Signature: ${awsHeaders.signature}")
 
-    awsHeaders.signature == newSignature.getOrElse("")
+    awsHeaders.signature == newSignature
   }
 
 }
