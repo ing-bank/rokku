b304059fd0f1e865bced0395e09454e4829f2c58
arempter <arempter@gmail.com>
2019-02-15 13:38:30 +0100

    integration test updated with embbeded kafka, application.conf added section

--- src/it/scala/com/ing/wbaa/airlock/proxy/provider/MessageProviderKafkaItTest.scala
+++ src/it/scala/com/ing/wbaa/airlock/proxy/provider/MessageProviderKafkaItTest.scala
@@ -2,70 +2,54 @@ package com.ing.wbaa.airlock.proxy.provider
 
 import java.net.InetAddress
 
-import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.{HttpMethods, RemoteAddress}
-import akka.kafka.Subscriptions
-import akka.kafka.scaladsl.Consumer
-import akka.kafka.testkit.scaladsl.{EmbeddedKafkaLike, ScalatestKafkaSpec}
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Keep, Sink}
 import com.ing.wbaa.airlock.proxy.config.KafkaSettings
-import com.ing.wbaa.airlock.proxy.data._
-import net.manub.embeddedkafka.EmbeddedKafkaConfig
-import org.scalatest.concurrent.{Eventually, ScalaFutures}
-import org.scalatest.{DiagrammedAssertions, Matchers, WordSpecLike}
+import com.ing.wbaa.airlock.proxy.data.{AwsAccessKey, AwsRequestCredential, Read, S3Request}
+import net.manub.embeddedkafka.{EmbeddedKafka, EmbeddedKafkaConfig}
+import org.scalatest.{DiagrammedAssertions, WordSpecLike}
 
-import scala.concurrent.duration._
+class MessageProviderKafkaItTest extends WordSpecLike with DiagrammedAssertions with EmbeddedKafka with MessageProviderKafka {
 
-abstract class SpecBase(kafkaPort: Int)
-  extends ScalatestKafkaSpec(kafkaPort)
-    with WordSpecLike
-    with EmbeddedKafkaLike
-    with Matchers
-    with ScalaFutures
-    with Eventually
-    with DiagrammedAssertions
-    with MessageProviderKafka
+  implicit val testSystem: ActorSystem = ActorSystem("kafkaTest")
 
+  override implicit def kafkaSettings: KafkaSettings = new KafkaSettings(testSystem.settings.config)
 
-class MessageProviderKafkaItTest extends SpecBase(9092) {
-
-  implicit val testSystem: ActorSystem = ActorSystem.create("test-system")
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
-  implicit val patience = PatienceConfig(15.seconds, 1.second)
-
-  override val kafkaSettings: KafkaSettings = new KafkaSettings(testSystem.settings.config) {
-    override val bootstrapServers: String = "127.0.0.1:9092"
-    override val kafkaEnabled: Boolean = true
-  }
+  override implicit val materializer: ActorMaterializer = ActorMaterializer()
 
   val s3Request = S3Request(AwsRequestCredential(AwsAccessKey("a"), None), Some("demobucket"), Some("s3object"), Read)
   val remoteClientIP = RemoteAddress(InetAddress.getByName("127.0.0.1"))
 
-  def createKafkaConfig: EmbeddedKafkaConfig =
-    EmbeddedKafkaConfig(kafkaPort,
-      zooKeeperPort,
-      Map(
-        "offsets.topic.replication.factor" -> "1"
-      ))
-
   "KafkaMessageProvider" should {
-    "Send message" in {
-      val createEventsTopic = createTopic(0,1,1)
-      val (control2, result) = Consumer
-        .plainSource(consumerDefaults.withGroupId(createGroupId()), Subscriptions.topics(createEventsTopic))
-        .toMat(Sink.seq)(Keep.both)
-        .run()
+    "Send message to correct topic with Put or Post" in {
+      implicit val config = EmbeddedKafkaConfig(kafkaPort = 9092)
 
-      sleep(6.seconds)
-      sendSingleMessage("messagePublished", createEventsTopic, producerDefaults).futureValue should be(Done)
+      withRunningKafka {
+        val createEventsTopic = "create_events"
+        createCustomTopic(createEventsTopic)
+
+        emitEvent(s3Request, HttpMethods.PUT, "testUser", remoteClientIP)
+        val result = consumeFirstStringMessageFrom(createEventsTopic)
+        assert(result.contains("s3:ObjectCreated:PUT"))
+      }
+    }
+
+    "Send message to correct topic with Delete" in {
+      implicit val config = EmbeddedKafkaConfig(kafkaPort = 9092)
+
+      withRunningKafka {
+        val deleteEventsTopic = "delete_events"
+        createCustomTopic(deleteEventsTopic)
+
+        emitEvent(s3Request, HttpMethods.DELETE, "testUser", remoteClientIP)
+        assert(consumeFirstStringMessageFrom(deleteEventsTopic).contains("s3:ObjectCreated:DELETE"))
+      }
     }
 
     "fail on incomplete data" in {
       assertThrows[Exception](emitEvent(s3Request.copy(s3Object = None), HttpMethods.PUT, "testUser", remoteClientIP))
     }
   }
-
 
 }
