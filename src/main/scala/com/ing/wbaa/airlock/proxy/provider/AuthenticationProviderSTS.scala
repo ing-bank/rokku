a674be7fadc494d54366543525b4e19db1beb950
Davide Dal Bianco <dal.bianco.davide@gmail.com>
2019-03-05 14:32:17 +0100

    WIP: Use JWT

--- src/main/scala/com/ing/wbaa/airlock/proxy/provider/AuthenticationProviderSTS.scala
+++ src/main/scala/com/ing/wbaa/airlock/proxy/provider/AuthenticationProviderSTS.scala
@@ -1,20 +1,32 @@
 package com.ing.wbaa.airlock.proxy.provider
 
+import java.security.Key
+
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ HttpRequest, StatusCodes, Uri }
-import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.stream.Materializer
 import com.ing.wbaa.airlock.proxy.config.StsSettings
-import com.ing.wbaa.airlock.proxy.data.{ AwsRequestCredential, JsonProtocols, User, UserRawJson }
+import com.ing.wbaa.airlock.proxy.data._
 import com.typesafe.scalalogging.LazyLogging
+import io.jsonwebtoken.Jwts
 
 import scala.concurrent.{ ExecutionContext, Future }
+import scala.util.Try
 
 trait AuthenticationProviderSTS extends JsonProtocols with LazyLogging {
 
-  import AuthenticationProviderSTS.STSException
-  import spray.json._
+  private val key: Key = {
+    import java.security.KeyFactory
+    import java.security.spec.X509EncodedKeySpec
+    import java.util.Base64
+
+    val publicKeyContent = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArQuAZEdjYst6u9+r3ZHiHeA/U3pDEopukbMCNlCvsIx7Li12MfJXqfg6XRY5Lxkk9l2Y2pRjrY23aFzvl0s6AkzeJfCATe711FRvRgeCQ1NBheI+bwCCrAvxZqiqywy2KRKCS7jGAoS+bveq+ScKdQgdqrFdRzxB8aUiDaZRfATyihJ5MaTttMv4COpkZgmsKtt8Q47sgpPf2G/uzyrLdMKuU/qcHTG2Z+bf+lSSmnL/H8m9IwdWqFEJkYZjxus5zQJdfEBEhh7BrY7MVVv90XVa4JveZtFGTDYfLo78MI8fsS1Y11m+IN2kZW05Umdl91VOz1ibHYwblw7S5fHdGQIDAQAB"
+
+    val kf = KeyFactory.getInstance("RSA")
+    val keySpec = new X509EncodedKeySpec(Base64.getDecoder.decode(publicKeyContent))
+    val pubKey = kf.generatePublic(keySpec)
+
+    pubKey
+  }
 
   protected[this] implicit def system: ActorSystem
   protected[this] implicit def executionContext: ExecutionContext
@@ -23,38 +35,20 @@ trait AuthenticationProviderSTS extends JsonProtocols with LazyLogging {
   protected[this] def stsSettings: StsSettings
 
   protected[this] def areCredentialsActive(awsRequestCredential: AwsRequestCredential): Future[Option[User]] = {
-    val QueryParameters =
-      Map("accessKey" -> awsRequestCredential.accessKey.value) ++
-        awsRequestCredential.sessionToken.map(s => "sessionToken" -> s.value)
+    val optToken = awsRequestCredential.sessionToken
 
-    val uri = stsSettings.stsBaseUri
-      .withPath(Uri.Path("/isCredentialActive"))
-      .withQuery(Uri.Query(QueryParameters))
-
-    Http()
-      .singleRequest(HttpRequest(uri = uri))
-      .flatMap { response =>
-        response.status match {
-
-          case StatusCodes.OK =>
-            Unmarshal(response.entity).to[String].map { jsonString =>
-              Some(User(jsonString.parseJson.convertTo[UserRawJson]))
-            }
-
-          case StatusCodes.Forbidden =>
-            logger.error(s"User not authenticated " +
-              s"with accessKey (${awsRequestCredential.accessKey.value}) " +
-              s"and sessionToken (${awsRequestCredential.sessionToken})")
-            Future.successful(None)
-
-          case c =>
-            val msg = s"Received unexpected StatusCode ($c) for " +
-              s"accessKey (${awsRequestCredential.accessKey.value}) " +
-              s"and sessionToken (${awsRequestCredential.sessionToken})"
-            logger.error(msg)
-            Future.failed(STSException(msg))
-        }
+    val user =
+      optToken flatMap { token =>
+        Try {
+          val claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token.value).getBody
+          val userName = UserName(claims.getSubject)
+          val userGroups = claims.get("user-groups", classOf[String]).split(':').map(UserGroup).toSet
+          val secretKey = AwsSecretKey(claims.get("secret-key", classOf[String]))
+          User(userName, userGroups, awsRequestCredential.accessKey, secretKey)
+        }.toOption
       }
+
+    Future.successful(user)
   }
 }
 
