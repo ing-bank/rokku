b9f16e3ad0e23c34eb76022df6050b51e08ff2e7
arempter <arempter@gmail.com>
2019-03-27 11:31:34 +0100

    bucket lineage and refactoring

--- src/main/scala/com/ing/wbaa/airlock/proxy/provider/LineageProviderAtlas.scala
+++ src/main/scala/com/ing/wbaa/airlock/proxy/provider/LineageProviderAtlas.scala
@@ -6,6 +6,8 @@ import akka.http.scaladsl.model.{ HttpMethods, HttpRequest, RemoteAddress }
 import com.ing.wbaa.airlock.proxy.config.AtlasSettings
 import com.ing.wbaa.airlock.proxy.data._
 import com.ing.wbaa.airlock.proxy.provider.atlas.LineageHelpers
+import com.ing.wbaa.airlock.proxy.provider.atlas.ModelKafka.bucketEntity
+import com.ing.wbaa.airlock.proxy.data.LineageLiterals._
 
 import scala.concurrent.Future
 
@@ -18,35 +20,45 @@ trait LineageProviderAtlas extends LineageHelpers {
     val lineageHeaders = getLineageHeaders(httpRequest)
     val bucketObject = lineageHeaders.bucketObject.getOrElse("emptyObject")
 
-    // we only report lineage for object operations. We do not track bucket create / delete etc.
-    if (lineageHeaders.bucket.length > 1 && bucketObject != "emptyObject") {
+    if (lineageHeaders.bucket.length > 1) {
       lineageHeaders.method match {
-        //todo: add case for make / remove bucket
+        // mb bucket
+        case HttpMethods.PUT if !lineageHeaders.bucket.isEmpty && bucketObject == "emptyObject" =>
+          createSingleEntity(lineageHeaders.bucket, userSTS, bucketEntity(lineageHeaders.bucket, userSTS.userName.value, newGuid), AWS_S3_BUCKET_TYPE)
+
+        // rm bucket
+        case HttpMethods.DELETE if !lineageHeaders.bucket.isEmpty && bucketObject == "emptyObject" =>
+          deleteEntityLineage(lineageHeaders.bucket, userSTS, AWS_S3_BUCKET_TYPE)
+          deleteEntityLineage(s"${lineageHeaders.bucket}/", userSTS, AWS_S3_PSEUDO_DIR_TYPE) // we also have to remove pseudodir root
 
         // get object
-        case HttpMethods.GET if lineageHeaders.queryParams.isEmpty || lineageHeaders.queryParams.contains("encoding-type") =>
+        case HttpMethods.GET if lineageHeaders.queryParams.isEmpty || lineageHeaders.queryParams.contains("encoding-type") && !bucketObject.isEmpty =>
           val externalObject = s"$EXTERNAL_OBJECT_OUT/${bucketObject.split("/").takeRight(1).mkString}"
           kafkaReadOrWriteLineage(lineageHeaders, userSTS, Read, clientIPAddress, Some(externalObject))
 
         // put object from outside of ceph
-        case HttpMethods.PUT if lineageHeaders.queryParams.isEmpty && lineageHeaders.copySource.isEmpty =>
+        case HttpMethods.PUT if lineageHeaders.queryParams.isEmpty && lineageHeaders.copySource.isEmpty && !bucketObject.isEmpty =>
           val externalObject = s"$EXTERNAL_OBJECT_IN/${bucketObject.split("/").takeRight(1).mkString}"
           kafkaReadOrWriteLineage(lineageHeaders, userSTS, Write, clientIPAddress, Some(externalObject))
 
         // put object - copy
         // if contains header x-amz-copy-source
-        case HttpMethods.PUT if lineageHeaders.copySource.getOrElse("").length > 0 => lineageForCopyOperation(lineageHeaders, userSTS, Write, clientIPAddress)
+        case HttpMethods.PUT if lineageHeaders.copySource.getOrElse("").length > 0 && !bucketObject.isEmpty =>
+          lineageForCopyOperation(lineageHeaders, userSTS, Write, clientIPAddress)
 
         // post object (complete multipart)
         // aws request eg. POST /ObjectName?uploadId=UploadId and content-type application/xml
-        case HttpMethods.POST if lineageHeaders.queryParams.getOrElse("").contains("uploadId") => kafkaReadOrWriteLineage(lineageHeaders, userSTS, Write, clientIPAddress)
+        case HttpMethods.POST if lineageHeaders.queryParams.getOrElse("").contains("uploadId") && !bucketObject.isEmpty =>
+          kafkaReadOrWriteLineage(lineageHeaders, userSTS, Write, clientIPAddress)
 
         // delete object
-        case HttpMethods.DELETE if lineageHeaders.queryParams.isEmpty => deleteEntityLineage(lineageHeaders, userSTS)
+        case HttpMethods.DELETE if lineageHeaders.queryParams.isEmpty && !bucketObject.isEmpty =>
+          deleteEntityLineage(lineageHeaders.bucketObject.getOrElse(""), userSTS)
 
         // delete on abort multipart
         // DELETE /ObjectName?uploadId=UploadId
-        case HttpMethods.DELETE if lineageHeaders.queryParams.getOrElse("").contains("uploadId") => deleteEntityLineage(lineageHeaders, userSTS)
+        case HttpMethods.DELETE if lineageHeaders.queryParams.getOrElse("").contains("uploadId") && !bucketObject.isEmpty =>
+          deleteEntityLineage(lineageHeaders.bucketObject.getOrElse(""), userSTS)
 
         case _ => Future.successful(Done)
       }
@@ -54,10 +66,4 @@ trait LineageProviderAtlas extends LineageHelpers {
       Future.successful(Done)
     }
   }
-}
-
-object LineageProviderAtlas {
-  final case class LineageProviderAtlasException(private val message: String, private val cause: Throwable = None.orNull)
-    extends Exception(message, cause)
-
 }
