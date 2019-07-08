e54c8a0de2c630979a25be0e854c113fd5cc3334
arempter <arempter@gmail.com>
2019-07-08 13:25:33 +0200

    cassandra configuration and tests added

--- src/main/scala/com/ing/wbaa/rokku/proxy/persistence/serializers/HttpRequestSerializer.scala
+++ src/main/scala/com/ing/wbaa/rokku/proxy/persistence/serializers/HttpRequestSerializer.scala
@@ -2,10 +2,10 @@ package com.ing.wbaa.rokku.proxy.persistence.serializers
 
 import java.nio.charset.Charset
 
-import akka.http.scaladsl.model.HttpEntity
+import akka.http.scaladsl.model.{ HttpEntity, HttpRequest, RemoteAddress }
 import akka.serialization.SerializerWithStringManifest
 import com.ing.wbaa.rokku.proxy.data.{ User, UserRawJson }
-import com.ing.wbaa.rokku.proxy.persistence.ExecutedRequestEvt
+import com.ing.wbaa.rokku.proxy.persistence.{ CurrentRequestsState, ExecutedRequestEvt }
 import spray.json._
 
 class HttpRequestSerializer extends SerializerWithStringManifest with HttpRequestConversionSupport {
@@ -13,48 +13,63 @@ class HttpRequestSerializer extends SerializerWithStringManifest with HttpReques
 
   val Utf8 = Charset.forName("UTF-8")
   val HttpRequestManifest = classOf[ExecutedRequestEvt].getName
+  val HttpRequestsManifest = classOf[CurrentRequestsState].getName
+
+  def simplifiedHttpRequestString(e: HttpRequest) =
+    SimplifiedHttpRequest(
+      e.method.value,
+      e.uri.toString(),
+      convertAkkaHeadersToStrings(e.headers),
+      HttpEntity.Empty.withContentType(e.entity.contentType).toString(),
+      e.protocol.value
+    ).toJson.toString
+
+  def userSTSString(u: User) =
+    UserRawJson(
+      u.userName.value,
+      u.userGroups.map(g => g.value),
+      u.accessKey.value,
+      u.secretKey.value).toJson.toString
+
+  def remoteIPString(a: RemoteAddress) = SimplifiedRemoteAddress(a.value).toJson.toString()
+
+  def toExecutedRequestEvt(r: String) = {
+    val Array(hr, u, ip) = r.split("[|]")
+    val httpRequest = toAkkaHttpRequest(hr.parseJson.convertTo[SimplifiedHttpRequest])
+    val userSTS = User(u.parseJson.convertTo[UserRawJson])
+    val simplifiedRemoteAddress = ip.parseJson.convertTo[SimplifiedRemoteAddress]
+
+    ExecutedRequestEvt(httpRequest, userSTS, simplifiedRemoteAddress.toRemoteAddr)
+  }
 
   override def manifest(o: AnyRef): String = o.getClass.getName
 
   override def toBinary(o: AnyRef): Array[Byte] =
     o match {
-      case re: ExecutedRequestEvt =>
-        val simplifiedHttpRequest =
-          SimplifiedHttpRequest(
-            re.httpRequest.method.value,
-            re.httpRequest.uri.toString(),
-            convertAkkaHeadersToStrings(re.httpRequest.headers),
-            HttpEntity.Empty.withContentType(re.httpRequest.entity.contentType).toString(),
-            re.httpRequest.protocol.value
-          ).toJson.toString
+      case r: ExecutedRequestEvt =>
+        s"${simplifiedHttpRequestString(r.httpRequest)}|${userSTSString(r.userSTS)}|${remoteIPString(r.clientIPAddress)}".getBytes(Utf8)
 
-        val userSTS = re.userSTS
-        val rawUserSTS =
-          UserRawJson(
-            userSTS.userName.value,
-            userSTS.userGroups.map(g => g.value),
-            userSTS.accessKey.value,
-            userSTS.secretKey.value).toJson.toString
-
-        val remoteIP = SimplifiedRemoteAddress(re.clientIPAddress.value).toJson.toString()
-
-        s"$simplifiedHttpRequest|$rawUserSTS|$remoteIP".getBytes(Utf8)
+      case c: CurrentRequestsState =>
+        c.requests.map { re =>
+          s"${simplifiedHttpRequestString(re.httpRequest)}|${userSTSString(re.userSTS)}|${remoteIPString(re.clientIPAddress)}"
+        }.mkString("|-").getBytes(Utf8)
 
       case e: IllegalArgumentException =>
         throw new IllegalArgumentException(s"Unable to serialize to bytes, class: ${o.getClass} ${e.getMessage}")
     }
 
-  override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef =
+  override def fromBinary(bytes: Array[Byte], manifest: String): AnyRef = {
     manifest match {
-      case HttpRequestManifest =>
+      case s: String if s == HttpRequestManifest =>
         val storedRequest = new String(bytes, Utf8)
-        val Array(hr, u, ip) = storedRequest.split("[|]")
-        val httpRequest = toAkkaHttpRequest(hr.parseJson.convertTo[SimplifiedHttpRequest])
-        val userSTS = User(u.parseJson.convertTo[UserRawJson])
-        val simplifiedRemoteAddress = ip.parseJson.convertTo[SimplifiedRemoteAddress]
+        toExecutedRequestEvt(storedRequest)
 
-        ExecutedRequestEvt(httpRequest, userSTS, simplifiedRemoteAddress.toRemoteAddr)
+      case s: String if s == HttpRequestsManifest =>
+        val storedRequests = new String(bytes, Utf8)
+        val requestsList: List[String] = storedRequests.split("[|-]{2}").toList
+        CurrentRequestsState(requestsList.map(toExecutedRequestEvt))
 
       case _ => throw new IllegalArgumentException(s"Unable to de-serialize from bytes for manifest: $manifest")
     }
+  }
 }
