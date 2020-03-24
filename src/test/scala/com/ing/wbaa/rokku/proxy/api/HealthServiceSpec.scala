4b436fc5e8485836b08a54ea1d2921a9ca283a98
AdamR <31927552+arempter@users.noreply.github.com>
2020-03-24 07:29:05 +0100

    Revert "Change health check"

--- src/test/scala/com/ing/wbaa/rokku/proxy/api/HealthServiceSpec.scala
+++ src/test/scala/com/ing/wbaa/rokku/proxy/api/HealthServiceSpec.scala
@@ -4,25 +4,21 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.ing.wbaa.rokku.proxy.config.StorageS3Settings
-import com.ing.wbaa.rokku.proxy.data.HealthCheck
-import com.ing.wbaa.rokku.proxy.data.HealthCheck.{ RGWListBuckets, S3ListBucket }
 import com.ing.wbaa.rokku.proxy.provider.aws.S3Client
-import org.scalatest.OneInstancePerTest
 import org.scalatest.diagrams.Diagrams
 import org.scalatest.flatspec.AnyFlatSpec
 
-class HealthServiceSpec extends AnyFlatSpec with ScalatestRouteTest with Diagrams with OneInstancePerTest {
+import scala.concurrent.ExecutionContext
+
+class HealthServiceSpec extends AnyFlatSpec with ScalatestRouteTest with Diagrams {
 
   private trait HealthServiceMock extends HealthService with S3Client {
     override implicit def system: ActorSystem = ActorSystem.create("test-system")
-    override def storageS3Settings: StorageS3Settings = StorageS3Settings(system)
+    override implicit def executionContext: ExecutionContext = system.dispatcher
   }
 
-  "A health service" should "respond to /ping with 'pong' when probe is RGWListBuckets" in new HealthServiceMock() {
-    override def storageS3Settings: StorageS3Settings = new StorageS3Settings(system.settings.config) {
-      override val hcMethod: HealthCheck.HCMethod = RGWListBuckets
-      override val hcInterval: Long = 0
-    }
+  "A health service" should "respond to /ping with 'pong'" in new HealthServiceMock() {
+    override protected[this] def storageS3Settings: StorageS3Settings = null
     override protected[this] def listAllBuckets: Seq[String] = List("a")
     Get("/ping") ~> healthRoute ~> check {
       assert(status == StatusCodes.OK)
@@ -30,35 +26,9 @@ class HealthServiceSpec extends AnyFlatSpec with ScalatestRouteTest with Diagram
     }
   }
 
-  it should "fail when storage returns an exception' when probe is RGWListBuckets" in new HealthServiceMock() {
-    override def storageS3Settings: StorageS3Settings = new StorageS3Settings(system.settings.config) {
-      override val hcMethod: HealthCheck.HCMethod = RGWListBuckets
-      override val hcInterval: Long = 0
-    }
-    override protected[this] def listAllBuckets: Seq[String] = throw new Exception("RGW not available")
-    Get("/ping") ~> healthRoute ~> check {
-      assert(status == StatusCodes.InternalServerError)
-    }
-  }
-
-  it should "respond to /ping with 'pong' when probe is S3ListBucket" in new HealthServiceMock() {
-    override def storageS3Settings: StorageS3Settings = new StorageS3Settings(system.settings.config) {
-      override val hcMethod: HealthCheck.HCMethod = S3ListBucket
-      override val hcInterval: Long = 0
-    }
-    override def listBucket: String = "rokku_hc_bucket"
-    Get("/ping") ~> healthRoute ~> check {
-      assert(status == StatusCodes.OK)
-      assert(responseAs[String] == "pong")
-    }
-  }
-
-  it should "fail when storage returns an exception' when probe is S3ListBucket" in new HealthServiceMock() {
-    override def storageS3Settings: StorageS3Settings = new StorageS3Settings(system.settings.config) {
-      override val hcMethod: HealthCheck.HCMethod = S3ListBucket
-      override val hcInterval: Long = 0
-    }
-    override def listBucket: String = throw new Exception("S3 not available")
+  "A health service" should "failed when storage returns an exception'" in new HealthServiceMock() {
+    override protected[this] def storageS3Settings: StorageS3Settings = null
+    override protected[this] def listAllBuckets: Seq[String] = throw new Exception()
     Get("/ping") ~> healthRoute ~> check {
       assert(status == StatusCodes.InternalServerError)
     }
