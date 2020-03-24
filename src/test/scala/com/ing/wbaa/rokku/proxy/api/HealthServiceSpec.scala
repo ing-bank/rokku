67ad324c7f636777493b3fc30b6f747c1e385260
arempter <arempter@gmail.com>
2020-03-24 17:38:39 +0100

    integrate hc tests into one

--- src/test/scala/com/ing/wbaa/rokku/proxy/api/HealthServiceSpec.scala
+++ src/test/scala/com/ing/wbaa/rokku/proxy/api/HealthServiceSpec.scala
@@ -1,7 +1,10 @@
 package com.ing.wbaa.rokku.proxy.api
 
+import java.util.concurrent.ForkJoinPool
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives.get
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import com.ing.wbaa.rokku.proxy.config.StorageS3Settings
 import com.ing.wbaa.rokku.proxy.data.HealthCheck
@@ -9,6 +12,8 @@ import com.ing.wbaa.rokku.proxy.data.HealthCheck.{ RGWListBuckets, S3ListBucket 
 import com.ing.wbaa.rokku.proxy.provider.aws.S3Client
 import org.scalatest.diagrams.Diagrams
 import org.scalatest.flatspec.AnyFlatSpec
+
+import scala.concurrent.ExecutionContext
 
 class HealthServiceSpec extends AnyFlatSpec with ScalatestRouteTest with Diagrams {
 
@@ -62,6 +67,41 @@ class HealthServiceSpec extends AnyFlatSpec with ScalatestRouteTest with Diagram
     Get("/ping") ~> healthRoute ~> check {
       assert(status == StatusCodes.InternalServerError)
     }
+
+  }
+  it should "return ok with concurrent requests" in new HealthServiceMock() {
+    override def storageS3Settings: StorageS3Settings = new StorageS3Settings(system.settings.config) {
+      override val hcMethod: HealthCheck.HCMethod = S3ListBucket
+      override val hcInterval: Long = 0
+    }
+    override def listBucket: String = "some_bucket"
+
+    private val threadPool = ExecutionContext.fromExecutor(new ForkJoinPool(32))
+
+    private def runProbe() =
+      new Runnable {
+        override def run(): Unit = {
+          Thread.sleep(100)
+          getStatus(System.currentTimeMillis())
+        }
+      }
+
+    for (_ <- 1 to 50) {
+      for (_ <- 1 to 50) {
+        threadPool.execute(runProbe())
+      }
+    }
+
+    Thread.sleep(1000)
+    val f = getStatus(System.currentTimeMillis())
+    val route = for {
+      r <- f
+    } yield get { r.get }
+
+    route.map(r =>
+      Get() ~> r ~> check {
+        assert(responseAs[String].contains("pong"))
+      })
 
   }
 }
