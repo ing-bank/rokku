ec5dd8d8b9662117c5e9991d1e187e3c67882685
Krzysztof Zmij <kr7ysztof@gmail.com>
2020-04-07 11:54:00 +0200

    parser for cachev1

--- src/main/scala/com/ing/wbaa/rokku/proxy/handler/parsers/RequestParser.scala
+++ src/main/scala/com/ing/wbaa/rokku/proxy/handler/parsers/RequestParser.scala
@@ -13,6 +13,7 @@ object RequestParser {
 
   case class MultipartRequestType(uploadId: String, completeMultipartUpload: Boolean) extends AWSRequestType
 
+  //
   class ModifyObjectRequestType() extends AWSRequestType
 
   case class GetObjectRequestType() extends AWSRequestType
@@ -25,19 +26,19 @@ object RequestParser {
 
   case class DeleteObjectRequestType() extends ModifyObjectRequestType
 
-  case class ListBucketsRequestType() extends ModifyObjectRequestType
+  case class ListBucketsRequestType() extends AWSRequestType
 
-  case class BucketOpsRequestType() extends ModifyObjectRequestType
+  case class BucketOpsRequestType() extends AWSRequestType
 
-  case class ObjectOpsRequestType() extends ModifyObjectRequestType
+  case class ObjectOpsRequestType() extends AWSRequestType
 
-  case class SubFolderOpsRequestType() extends ModifyObjectRequestType
+  case class SubFolderOpsRequestType() extends AWSRequestType
 
   case class MultiDeleteRequestType() extends ModifyObjectRequestType
 
-  case class HeadObjectRequestType() extends ModifyObjectRequestType
+  case class HeadObjectRequestType() extends AWSRequestType
 
-  case class HeadBucketRequestType() extends ModifyObjectRequestType
+  case class HeadBucketRequestType() extends AWSRequestType
 }
 
 trait RequestParser {
@@ -57,7 +58,7 @@ trait RequestParser {
     val pathString = request.uri.path.toString()
     val s3path = S3Utils.getS3PathWithoutBucketName(pathString)
     val s3Object = S3Utils.getS3FullObjectPath(pathString)
-    val s3Bucket = S3Utils.getBucketName(pathString)
+    //    val s3Bucket = S3Utils.getBucketName(pathString)
 
     method match {
       // aws multipart part upload eg. PUT /ObjectName?uploadId=UploadId
@@ -66,39 +67,13 @@ trait RequestParser {
       // aws multipart complete eg. POST /ObjectName?uploadId=UploadId and content-type application/xml
       case HttpMethods.POST if containsUploadId && (isXML || isOctetStream) => MultipartRequestType(uploadId(queryString), completeMultipartUpload = true)
 
-      //TODO verify create right rules
-      // if queryString is empty multipart os are not covered
-      case HttpMethods.GET if queryString.isEmpty => GetObjectRequestType()
-      case HttpMethods.PUT if queryString.isEmpty => PutObjectRequestType()
-      case HttpMethods.POST if queryString.isEmpty => PostObjectRequestType() // I think it is not needed if no queryString?
-      case HttpMethods.DELETE if queryString.isEmpty => DeleteObjectRequestType()
-
-      // object operations, put / delete etc.
-      //case S3Request(_, Some(s3path), Some(_), _, _, _, _) => GetOrPutObjectRequestType()
-      case HttpMethods.GET | HttpMethods.DELETE | HttpMethods.PUT if s3path.isDefined && s3Object.isDefined => ObjectOpsRequestType()
-
-      // object operation as subfolder, in this case object can be empty
-      // we need this to differentiate subfolder create/delete from bucket create/delete
-      case HttpMethods.GET | HttpMethods.DELETE | HttpMethods.PUT if s3path.isDefined && !s3Object.isDefined && s3path.get.endsWith("/") => SubFolderOpsRequestType()
-
-      // list-objects in the bucket operation
-      case HttpMethods.GET if s3path.isDefined && !s3Object.isDefined => ListObjectsRequestType()
-
-      // multidelete with xml list of objects in post
-      case HttpMethods.POST if s3path.isDefined && !s3Object.isDefined && (isXML || isOctetStream) => MultiDeleteRequestType()
-
-      // list / create / delete bucket operation
-      case HttpMethods.GET | HttpMethods.PUT | HttpMethods.DELETE if !s3Bucket.isEmpty && !s3Object.isDefined => BucketOpsRequestType()
-
-      // list buckets
-      case HttpMethods.GET if !s3path.isDefined && !s3Object.isDefined => ListBucketsRequestType()
-
-      // head bucket
-      case HttpMethods.HEAD if s3path.isDefined && !s3Object.isDefined => HeadBucketRequestType()
-
-      // head bucket
-      case HttpMethods.HEAD if s3path.isDefined && s3Object.isDefined => HeadObjectRequestType()
-
+      //for cacheRulesV1 - we cache only Get object and need to know when a modification object happens
+      case HttpMethods.GET if s3path.isDefined && s3Object.isDefined => GetObjectRequestType()
+      //for cacheRulesV1 - we need to know when a modification object happens to invalidate cache
+      //TODO do it more specific (object/bucket/dir...)
+      case HttpMethods.PUT => PutObjectRequestType()
+      case HttpMethods.POST => PostObjectRequestType()
+      case HttpMethods.DELETE => DeleteObjectRequestType()
       case _ => RequestTypeUnknown()
     }
   }
