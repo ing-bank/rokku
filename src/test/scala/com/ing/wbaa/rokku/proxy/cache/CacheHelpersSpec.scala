d519827558008e14467be5b11b4213be685e4dee
arempter <arempter@gmail.com>
2020-04-28 14:32:18 +0200

    fix fakeEntity size

---
+++ src/test/scala/com/ing/wbaa/rokku/proxy/cache/CacheHelpersSpec.scala
@@ -0,0 +1,15 @@
+package com.ing.wbaa.rokku.proxy.cache
+
+import com.ing.wbaa.rokku.proxy.handler.parsers.CacheHelpers._
+import org.scalatest.diagrams.Diagrams
+import org.scalatest.wordspec.AnyWordSpec
+
+class CacheHelpersSpec extends AnyWordSpec with Diagrams {
+
+  "Hazelcast Cache helpers" should {
+    "returns non empty Entity if incorrect CL" in {
+      assert(generateFakeEntity(0).contentLength == 1)
+    }
+  }
+
+}
