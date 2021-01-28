2f94592f3e93d3c504995fd06543963d0919aeff
Krzysztof Zmij <kr7ysztof@gmail.com>
2021-01-28 20:03:25 +0100

    Revert "remove unused cache settings"
    This reverts commit 72b808403cb25fe90d52e0dc300d11e5a240840c.

--- src/main/scala/com/ing/wbaa/rokku/proxy/config/StorageS3Settings.scala
+++ src/main/scala/com/ing/wbaa/rokku/proxy/config/StorageS3Settings.scala
@@ -20,6 +20,8 @@ class StorageS3Settings(config: Config) extends Extension {
     case "rgwListBuckets" => RGWListBuckets
     case "s3ListBucket"   => S3ListBucket
   }
+  val hcInterval: Long = config.getLong("rokku.storage.s3.healthCheck.interval")
+  val bucketName: String = config.getString("rokku.storage.s3.healthCheck.bucketName")
 }
 
 object StorageS3Settings extends ExtensionId[StorageS3Settings] with ExtensionIdProvider {
